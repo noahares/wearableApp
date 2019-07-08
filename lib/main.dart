9eb36202e26db29150d6897bb083f067b6220aaf
Noah Wahl <noah.ares@yahoo.de>
2019-07-08 14:28:59 +0200

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       color: Colors.lightBlue,
       home: StreamBuilder<BluetoothState>(
           stream: FlutterBlue.instance.state,
